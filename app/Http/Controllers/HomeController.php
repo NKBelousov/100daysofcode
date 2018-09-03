276d5e2720cbf47da85a99c50cc6c67ae5219d34
NKBelousov <belousovnikita92@gmail.com>
2018-09-03 21:06:46 +0300

    добавляет роут для просмотра профиля

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,14 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    /**
+     * Show current user profile
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function profile()
+    {
+        return view('profile');
+    }
 }
