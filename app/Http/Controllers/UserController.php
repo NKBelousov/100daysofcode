32379118ee989984d04ae70386aec220a00d53fd
NKBelousov <belousovnikita92@gmail.com>
2018-09-06 02:23:41 +0300

    добавляет простейший метод создания пользователей

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -14,5 +17,30 @@ class UserController extends Controller
     public function profile()
     {
         return view('profile');
+    }
+
+    /**
+     * Метод для создания нового пользователя
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $response = new JsonResponse();
+        $response->withHeaders([
+            'Content-Type' => 'application/json'
+        ]);
+        $method = $request->method();
+        if ($method !== "POST"){
+            $response->setData("Некорректный метод для создания пользователя: $method");
+        } else {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+            $response->setData($user);
+        }
+        return $response;
     }
 }
