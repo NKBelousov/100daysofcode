ea04993fdfe3dacb2a0a7c98ca0e833c82607887
NKBelousov <belousovnikita92@gmail.com>
2018-09-05 23:47:30 +0300

    добавляет спиннер в прелоадер

--- resources/assets/js/components/Preloader.vue
+++ resources/assets/js/components/Preloader.vue
@@ -16,12 +16,34 @@
 </script>
 
 <style scoped>
+    @keyframes rotate {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
     .wrapper {
         background: #fff;
         border: 2px solid #bebebe;
         border-radius: 5px;
         padding: 20px;
         text-align: center;
+    }
+    .wrapper:before {
+        animation: rotate 1s infinite ease;
+        border-color: #eee;
+        border-radius: 100%;
+        border-style: solid;
+        border-top-color: #3097D1;
+        border-width: 1px;
+        content: '';
+        display: block;
+        margin: 0 auto 5px;
+        height: 3rem;
+        transform: translate(0, 0.5rem);
+        width: 3rem;
     }
     .wrapper:not(:first-child){
         margin-top: 20px;
