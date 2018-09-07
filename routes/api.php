3cd1d1596107d27ebe54b4b0195a947fa1c7c9b0
NKBelousov <belousovnikita92@gmail.com>
2018-09-07 22:29:01 +0300

    добавляет метод получения пользователя по ID в роутинг

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,6 @@ use Illuminate\Http\Request;
 
 Route::delete('/user/{id}', 'UserController@delete');
 Route::get('/user', 'UserController@list');
+Route::get('/user/{id}', 'UserController@get');
 Route::post('/user', 'UserController@create');
 Route::put('/user/{id}', 'UserController@update');
