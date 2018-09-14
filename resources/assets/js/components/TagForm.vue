62adf8093e90d4b7ae301baf3424d487da56f641
NKBelousov <belousovnikita92@gmail.com>
2018-09-14 20:39:43 +0300

    добавляет простейшую форму для создания тегов

---
+++ resources/assets/js/components/TagForm.vue
@@ -0,0 +1,34 @@
+<template>
+    <form @submit="onSubmit($event)">
+      <field label="Тег" @input="onChange" :value="name"></Field>
+      <button type="submit">Сохранить</button>
+    </form>
+</template>
+
+<script>
+const Field = require("./Field.vue");
+const TagService = require("./../utils/TagService").default;
+
+export default {
+  data: function() {
+    return {
+      name: "",
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      TagService.save({
+        name: this.name,
+      });
+    },
+    onChange(value) {
+      this.name = value;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
