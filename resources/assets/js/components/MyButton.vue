903c46eb08d735419b1050c8551ef92c488bb8a4
NKBelousov <belousovnikita92@gmail.com>
2018-09-16 12:43:11 +0300

    добавляет компонент кнопки

---
+++ resources/assets/js/components/MyButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <button type="button" role="button">
+        <slot></slot>
+    </button>
+</template>
+
+<style>
+button {
+  background: #3097d1;
+  color: #fff;
+  padding: 5px;
+  margin: 0 auto;
+  border: none;
+}
+button:disabled {
+  background: grey;
+  color: #fff;
+  cursor: not-allowed;
+}
+button:focus {
+  font-weight: bold;
+}
+button:hover {
+  background: #fff;
+  color: #3097d1;
+}
+</style>
