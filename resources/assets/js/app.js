85e21ccbc357e60575ab420d939834b4f9e61bf6
NKBelousov <belousovnikita92@gmail.com>
2018-10-02 23:28:50 +0300

    добавляет в зависимости Vuex

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueMaterial from "vue-material";
+import Vuex from "vuex";
 
 import "./bootstrap";
 import MemeForm from "./components/meme-form.vue";
@@ -8,6 +9,7 @@ import TheProfile from "./components/the-profile.vue";
 import UserList from "./components/the-user-list.vue";
 
 Vue.use(VueMaterial);
+Vue.use(Vuex);
 Vue.component("meme-form", MemeForm);
 Vue.component("tag-form", TagForm);
 Vue.component("the-profile", TheProfile);
@@ -15,5 +17,5 @@ Vue.component("the-user-list", UserList);
 window.Vue = require("vue");
 
 const app = new Vue({
-  el: "#app",
+    el: "#app",
 });
