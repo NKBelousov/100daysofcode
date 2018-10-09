7144a3ddeff7d03d9b8e97dd0e14e1206c3a36d4
NKBelousov <belousovnikita92@gmail.com>
2018-10-09 22:14:13 +0300

    добавляет проверку на наличие оценок при проставлении новой

--- app/Http/Controllers/GradeController.php
+++ app/Http/Controllers/GradeController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+
+use App\Grade;
 
 class GradeController extends CrudController
 {
@@ -14,5 +17,36 @@ class GradeController extends CrudController
         $data = $request->all();
         $data["user_id"] = Auth::user()->id;
         return $data;
+    }
+
+    /**
+     * Метод для создания новой сущности
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function create(Request $request)
+    {
+        $data = $this->getModelParams($request);
+        $user = Auth::user();
+        $grades = Grade::where([
+            'user_id' => $user->id,
+            'meme_id' => $data['meme_id'],
+        ]);
+        if (count($grades) > 0) {
+            return new JsonResponse([
+                'error' => 'already_exists',
+            ], 400);
+        }
+        try {
+            $data = $this::getModelParams($request);
+            $model = $this->modelClass::create($data);
+            if ($model->save()) {
+                return new JsonResponse($model, 200);
+            } else {
+                return new JsonResponse(null, 400);
+            }
+        } catch (\Illuminate\Database\QueryException $e) {
+            return new JsonResponse($e, 400);
+        }
     }
 }
