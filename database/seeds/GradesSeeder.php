13a279a5349e42c2c39a8cc95e5827f5d62c975f
NKBelousov <belousovnikita92@gmail.com>
2018-10-17 22:43:57 +0300

    добавляет генератор для пользовательских оценок

---
+++ database/seeds/GradesSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\User;
+use App\Meme;
+
+class GradesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $randomUser = User::inRandomOrder()->first();
+        $randomMeme = Meme::inRandomOrder()->first();
+        $randomValue = rand() > 0.5 ? "positive" : "negative";
+        DB::table('grades')->insert([
+            'value' => $randomValue,
+            'user_id' => $randomUser->id,
+            'meme_id' => $randomMeme->id,
+        ]);
+    }
+}
