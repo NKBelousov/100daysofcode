7399d71c483d2869f357228a521433882b4e4c75
NKBelousov <belousovnikita92@gmail.com>
2018-10-26 22:20:30 +0300

    реализует метод для добавления в избранное

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -16,4 +16,35 @@ class FavoriteController extends CrudController
         $data["user_id"] = Auth::user()->id;
         return $data;
     }
+
+    /**
+     * Метод для создания новой сущности
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function create(Request $request)
+    {
+        $data = $this->getModelParams($request);
+        $user = Auth::user();
+        $grade = Favorite::where([
+            'user_id' => $user->id,
+            'meme_id' => $data['meme_id'],
+        ])->first();
+        if (empty($grade) === false) {
+            return new JsonResponse([
+                'error' => 'already_exists',
+            ], 400);
+        }
+        try {
+            $data = $this::getModelParams($request);
+            $model = $this->modelClass::create($data);
+            if ($model->save()) {
+                return new JsonResponse($model, 200);
+            } else {
+                return new JsonResponse(null, 400);
+            }
+        } catch (\Illuminate\Database\QueryException $e) {
+            return new JsonResponse($e, 400);
+        }
+    }
 }
