665ab955bc4e0fb8e09c2e51afc6315bfa020588
NKBelousov <belousovnikita92@gmail.com>
2018-11-03 17:26:19 +0300

    удаляет неиспользуемый deepFreeze

--- resources/assets/js/utils/Request.js
+++ resources/assets/js/utils/Request.js
@@ -1,40 +1,37 @@
-import deepFreeze from "./../helpers/deepFreeze";
-
-export const DEFAULT = deepFreeze({
-    page: 1,
-    perPage: 10,
-    search: "",
+export const DEFAULT = Object.freeze({
+  page: 1,
+  perPage: 10,
+  search: "",
 });
 
 export default class Request {
-    constructor(config = {}) {
-        this.query = {
-            page: config.page || DEFAULT.page,
-            perPage: config.perPage || DEFAULT.perPage,
-            search: config.search || DEFAULT.search,
-        }
-    }
-    page(index) {
-        this.query.page = index;
-        return this;
-    }
-    prevPage() {
-        this.query.page = Math.max(this.query.page - 1, 0);
-        return this;
-    }
-    nextPage() {
-        this.query.page = Math.min(this.query.page + 1, Number.MAX_SAFE_INTEGER);
-        return this;
-    }
-    perPage(count) {
-        this.query.perPage = count;
-        return this;
-    }
-    search(what) {
-        this.query.search = what;
-        return this;
-    }
-    toQuery() {
-        return this.query;
-    }
-};
+  constructor(config = {}) {
+    this.query = {
+      ...DEFAULT,
+      ...config,
+    };
+  }
+  page(index) {
+    this.query.page = index;
+    return this;
+  }
+  prevPage() {
+    this.query.page = Math.max(this.query.page - 1, 0);
+    return this;
+  }
+  nextPage() {
+    this.query.page = Math.min(this.query.page + 1, Number.MAX_SAFE_INTEGER);
+    return this;
+  }
+  perPage(count) {
+    this.query.perPage = count;
+    return this;
+  }
+  search(what) {
+    this.query.search = what;
+    return this;
+  }
+  toQuery() {
+    return this.query;
+  }
+}
--- resources/assets/js/utils/Service.js
+++ resources/assets/js/utils/Service.js
@@ -1,37 +1,37 @@
 import axios from "axios";
 
-import Request, { DEFAULT } from "./Request";
+import Request from "./Request";
 
 const axiosInstance = window.axios || axios;
 
 export default class Service {
-    constructor(base) {
-        if (typeof base !== "string" || base === "") {
-            throw new Error("Service::В качестве базового URL ожидалась строка")
-        }
-        this.base = base;
-        this.http = axiosInstance;
+  constructor(base) {
+    if (typeof base !== "string" || base === "") {
+      throw new Error("Service::В качестве базового URL ожидалась строка")
     }
-    get(id) {
-        return this.http.get(`${this.base}/${id}`).then(response => response.data);
-    }
-    delete(id) {
-        return this.http.delete(`${this.base}/${id}`).then(response => response.data);
-    }
-    save(payload) {
-        const { id } = payload;
-        const isNew = typeof id !== "string" || id === "";
-        const url = isNew ? this.base : `${this.base}/${id}`;
-        const method = isNew ? "POST" : "PUT";
-        return this.http.request({
-            method,
-            data: payload,
-            url,
-        }).then(response => response.data);
-    }
-    list(request = new Request(DEFAULT)) {
-        return this.http.get(`${this.base}`, {
-            params: request.toQuery(),
-        }).then(response => response.data);
-    }
+    this.base = base;
+    this.http = axiosInstance;
+  }
+  get(id) {
+    return this.http.get(`${this.base}/${id}`).then(response => response.data);
+  }
+  delete(id) {
+    return this.http.delete(`${this.base}/${id}`).then(response => response.data);
+  }
+  save(payload) {
+    const { id } = payload;
+    const isNew = typeof id !== "string" || id === "";
+    const url = isNew ? this.base : `${this.base}/${id}`;
+    const method = isNew ? "POST" : "PUT";
+    return this.http.request({
+      method,
+      data: payload,
+      url,
+    }).then(response => response.data);
+  }
+  list(request = new Request()) {
+    return this.http.get(`${this.base}`, {
+      params: request.toQuery(),
+    }).then(response => response.data);
+  }
 };
