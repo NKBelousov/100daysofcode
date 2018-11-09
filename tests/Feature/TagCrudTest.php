a9d3ab4c374278b8b86cc196d3ae4a14df3d078c
NKBelousov <belousovnikita92@gmail.com>
2018-11-09 20:58:14 +0300

    добавляет тесты для контроллера тегов

---
+++ tests/Feature/TagCrudTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Http\Request;
+use Tests\TestCase;
+
+use App\Http\Controllers\TagController;
+use App\Tag;
+
+class TagCrudTest extends TestCase
+{
+    private function generateGetRequest()
+    {
+        $request = new Request();
+        return $request;
+    }
+
+    private function getListResponse()
+    {
+        $controller = new TagController();
+        $response = $controller->list($this->generateGetRequest());
+        $collection = json_decode($response->getContent());
+        return $collection;
+    }
+
+    public function testListHasArrayData()
+    {
+        $collection = $this->getListResponse();
+        $this->assertTrue(is_array($collection->data));
+    }
+
+    public function testListHasPageMetadata()
+    {
+        $collection = $this->getListResponse();
+        $this->assertTrue(is_numeric($collection->current_page));
+        $this->assertTrue(is_numeric($collection->per_page));
+        $this->assertTrue(is_numeric($collection->total));
+    }
+
+    public function testGetReturnsExistingModel()
+    {
+        $controller = new TagController();
+        $id = Tag::inRandomOrder()->first()->id;
+        $response = $controller->get($id);
+        $model = json_decode($response->getContent());
+        $this->assertNotEmpty($model);
+    }
+
+    public function testGetReturnsNotFoundForNonExistingModel()
+    {
+        $controller = new TagController();
+        $id = Tag::orderBy('id', 'desc')->first()->id + 1;
+        $response = $controller->get($id);
+        $this->assertEquals($response->getStatusCode(), 404);
+    }
+
+    public function testShouldPreventGetRequestWithoutId()
+    {
+        $controller = new TagController();
+        $response = $controller->get();
+        $this->assertEquals($response->getStatusCode(), 400);
+    }
+}
