84ee2085f886d63c6eef831f075a32659eabef10
NKBelousov <belousovnikita92@gmail.com>
2018-11-15 22:47:53 +0300

    исправляет доступ к web роутам

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('feed');
-});
-
 Auth::routes();
 
-Route::get('/feed', 'UserController@feed')->name('feed');
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/memes', 'MemeController@memes')->name('memes');
-Route::get('/profile', 'UserController@profile')->name('profile');
-Route::get('/tags', 'TagController@tags')->name('tags');
-Route::get('/users', 'UserController@users')->name('users');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/memes', 'MemeController@memes')->name('memes');
+    Route::get('/profile', 'UserController@profile')->name('profile');
+    Route::get('/tags', 'TagController@tags')->name('tags');
+    Route::get('/users', 'UserController@users')->name('users');
+    Route::get('/{feed?}', 'UserController@feed')->name('feed');
+});
