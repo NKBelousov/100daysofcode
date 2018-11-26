b7add38da3ad5a9287120f42d7d10e62695372d7
NKBelousov <belousovnikita92@gmail.com>
2018-11-26 23:15:39 +0300

    сохраняет теги вместе с мемом

--- resources/assets/js/utils/MemeService.js
+++ resources/assets/js/utils/MemeService.js
@@ -1,5 +1,24 @@
 import Service from "./Service";
 
-class MemeService extends Service { }
+class MemeService extends Service {
+  async save(payload) {
+    const data = {
+      title: payload.title,
+      description: payload.description,
+    };
+    const meme = await super.save(payload);
+    if (payload.tags.length > 0) {
+      await this.updateTags(meme.id, payload.tags);
+    }
+    return meme;
+  }
+  updateTags(memeId, tags) {
+    return this.http.request({
+      data: tags,
+      method: "PATCH",
+      url: `/api/meme/${memeId}/tags`,
+    }).then(response => response.data);
+  }
+}
 
 export default new MemeService(`/api/meme`);
