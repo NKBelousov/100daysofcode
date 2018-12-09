39377e4c408aa6fad07347b137ab3e56acedfeb9
NKBelousov <belousovnikita92@gmail.com>
2018-12-09 20:43:10 +0300

    добавляет уведомление при сохранении профиля

--- resources/assets/js/components/the-profile.vue
+++ resources/assets/js/components/the-profile.vue
@@ -1,10 +1,10 @@
 <template>
   <form class="md-layout" @submit.prevent="save">
     <md-dialog-alert
-      :md-active.sync="showDialog"
-      @md-confirm="showDialog = false"
-      md-content="Введенные пароли пусты или не совпадают"
-      md-title="Упс..."
+      :md-active.sync="dialog.visible"
+      :md-content="dialog.message"
+      :md-title="dialog.title"
+      @md-confirm="dialog.visible = false"
     />
     <md-card class="md-layout-item">
       <md-card-header>
@@ -13,23 +13,47 @@
       <md-card-content>
         <md-field>
           <label for="name">Имя пользователя:</label>
-          <md-input name="name" id="name" autocomplete="name" v-model="data.name" :disabled="isLoading" />
+          <md-input
+            name="name"
+            id="name"
+            autocomplete="name"
+            v-model="data.name"
+            :disabled="isLoading"
+          />
         </md-field>
         <md-field>
           <label for="email">E-mail:</label>
-          <md-input name="email" id="email" autocomplete="email" v-model="data.email" :disabled="isLoading" />
+          <md-input
+            name="email"
+            id="email"
+            autocomplete="email"
+            v-model="data.email"
+            :disabled="isLoading"
+          />
         </md-field>
         <md-field>
           <label for="password">Пароль:</label>
-          <md-input name="password" id="password" autocomplete="none" v-model="data.password" :disabled="isLoading" type="password"/>
+          <md-input
+            name="password"
+            id="password"
+            autocomplete="none"
+            v-model="data.password"
+            :disabled="isLoading"
+            type="password"
+          />
         </md-field>
         <md-field>
           <label for="passwordConfirmation">Повторите пароль:</label>
-          <md-input name="passwordConfirmation" id="passwordConfirmation" autocomplete="none" v-model="data.passwordConfirmation" :disabled="isLoading" type="password"/>
+          <md-input
+            name="passwordConfirmation"
+            id="passwordConfirmation"
+            autocomplete="none"
+            v-model="data.passwordConfirmation"
+            :disabled="isLoading"
+            type="password"
+          />
         </md-field>
-        <md-button class="md-raised md-primary" :disabled="isLoading" type="submit">
-          Сохранить
-        </md-button>
+        <md-button class="md-raised md-primary" :disabled="isLoading" type="submit">Сохранить</md-button>
       </md-card-content>
     </md-card>
   </form>
@@ -48,7 +72,11 @@ export default {
         password: "",
         passwordConfirmation: "",
       },
-      showDialog: false,
+      dialog: {
+        visible: false,
+        message: "",
+        title: "",
+      },
       isLoading: true,
     };
   },
@@ -64,12 +92,17 @@ export default {
         this.data.password === "" ||
         this.data.password !== this.data.passwordConfirmation
       ) {
-        this.showDialog = true;
+        this.dialog.visible = true;
+        this.dialog.message = "Введенные пароли пусты или не совпадают";
+        this.dialog.title = "Упс...";
         return;
       }
       this.isLoading = true;
       UserService.save(this.data)
         .then(() => {
+          this.dialog.visible = true;
+          this.dialog.message = "Профиль успешно изменен";
+          this.dialog.title = "Ура!";
           this.isLoading = false;
         })
         .catch(() => {
